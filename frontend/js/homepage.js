6419ec1e94a4b4833ba543b6e991413885e2acf8
Steven-Ao <stevenwz113@gmail.com>
2023-11-10 12:57:54 -0800

    added homepage template

---
+++ frontend/js/homepage.js
@@ -0,0 +1,48 @@
+function addViewedPapers() {
+    var ul = document.getElementById("recent");
+    console.log(ul);
+    var li = document.createElement("li");
+    li.setAttribute("class", "published")
+    console.log(li);
+
+    var a = document.createElement("a");
+    // change link later
+    a.href = "https://example.com";
+    a.appendChild(li);
+
+    var img = document.createElement("img");
+    img.src = "../icons/papers_icon.png";
+    li.appendChild(img);
+    var p = document.createElement("p");
+    p.innerHTML = "recent published paper example #1";
+    li.appendChild(p);
+    ul.insertBefore(a, ul.childNodes[0]);
+    console.log('added');
+}
+
+function addNewPapers() {
+    var ul = document.getElementById("new");
+    console.log(ul);
+    var li = document.createElement("li");
+    li.setAttribute("class", "published")
+    console.log(li);
+
+    var a = document.createElement("a");
+    // change link later
+    a.href = "https://example.com";
+    a.appendChild(li);
+
+    var img = document.createElement("img");
+    img.src = "../icons/papers_icon.png";
+    li.appendChild(img);
+    var p = document.createElement("p");
+    p.innerHTML = "recent published paper example #1";
+    li.appendChild(p);
+    ul.insertBefore(a, ul.childNodes[0]);
+    console.log('added');
+}
+
+for (let i = 0; i < 5; i++) {
+    addViewedPapers();
+    addNewPapers();
+  }
