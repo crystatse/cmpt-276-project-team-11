4de3d71217eb08d398fa41c6c29df4472a4b170b
Steven-Ao <stevenwz113@gmail.com>
2023-11-13 11:39:55 -0800

    fixed minor stuff

--- frontend/public/homepage/homepage.js
+++ frontend/public/homepage/homepage.js
@@ -1,54 +1,68 @@
+//temporary
+
 function addViewedPapers() {
     var ul = document.getElementById("recent");
-    console.log(ul);
     var li = document.createElement("li");
     li.setAttribute("class", "published")
-    console.log(li);
 
     var a = document.createElement("a");
-    // change link later
-    a.href = "https://example.com";
+    a.href = "chatbot.html";
     a.appendChild(li);
 
     var img = document.createElement("img");
-    img.src = "../icons/papers_icon.png";
+    img.src = "homepage/icons/papers_icon.png";
     li.appendChild(img);
+
+    // to be changed
     var p = document.createElement("p");
     p.innerHTML = "recent published paper example #1";
     li.appendChild(p);
+
     ul.insertBefore(a, ul.childNodes[0]);
     console.log('added');
 }
 
 function addNewPapers() {
     var ul = document.getElementById("new");
-    console.log(ul);
     var li = document.createElement("li");
     li.setAttribute("class", "published")
-    console.log(li);
 
     var a = document.createElement("a");
-    // change link later
-    a.href = "https://example.com";
+    a.href = "chatbot.html";
     a.appendChild(li);
 
     var img = document.createElement("img");
-    img.src = "../icons/papers_icon.png";
+    img.src = "homepage/icons/papers_icon.png";
     li.appendChild(img);
+
+    // to be changed
     var p = document.createElement("p");
     p.innerHTML = "recent published paper example #1";
     li.appendChild(p);
+
     ul.insertBefore(a, ul.childNodes[0]);
     console.log('added');
 }
 
-for (let i = 0; i < 5; i++) {
-    addViewedPapers();
-    addNewPapers();
-  }
 
-  var searchBar = document.getElementById('search');
-  searchBar.addEventListener('input', function() {
-      // Save input data to localStorage
-      localStorage.setItem('searchQuery', searchBar.value);
-  });
+//temporary testing functionality
+for (let i = 0; i < 13; i++) {
+    addViewedPapers();
+}
+var total = 13;
+var j = 0
+for (j; j < 7; j++) {
+    addNewPapers();
+}
+
+
+
+var searchBar = document.getElementById('search');
+searchBar.addEventListener('keydown', function(e) {
+
+    if (e.key === 'Enter') {
+        // Save input data to localStorage
+        localStorage.setItem('searchQuery', searchBar.value);
+    }
+
+});
