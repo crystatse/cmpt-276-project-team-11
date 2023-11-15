8962c42c4db0aec78638ae2627ee8df55f869adc
= <=>
2023-11-15 11:39:04 -0800

    added functionality to search between webpages and finalized styling closes #2

---
+++ frontend/js/about.js
@@ -0,0 +1,20 @@
+var inputElement = document.getElementById("search-bar");
+
+inputElement.addEventListener("keydown", function (event) {
+    // Check if the pressed key is Enter (key code 13)
+    if (event.keyCode === 13) {
+        var search = document.getElementById("search-bar").value;
+        if (search !== null && search.trim() !== "") {
+            localStorage.setItem("searchValue", search);
+            console.log("added search value to localStorage");
+        }
+
+        // Construct the URL for the destination HTML file
+        var destinationURL = "../public/searchresults.html";
+
+        // Redirect to the destination HTML file first
+        window.location.href = destinationURL;
+
+        event.preventDefault();
+    }
+});
