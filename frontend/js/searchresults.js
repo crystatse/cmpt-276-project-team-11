4e53ecccd723b0303c5def9ab02fbf7dcbd28af5
gurveeng <gurveenngill@gmail.com>
2023-11-15 18:00:50 -0800

    fixed image display on searchresults.html

--- frontend/js/searchresults.js
+++ frontend/js/searchresults.js
@@ -106,7 +106,6 @@ function searchArXiv(search) {
 }
 
 function displayResults(results) {
-    
     var resultsContainer = document.getElementById("search-results-container");
     var buttonsContainer = document.getElementById("article-button-container");
 
@@ -117,12 +116,19 @@ function displayResults(results) {
     // Create and append new elements for each result
     results.forEach(result => {
         var resultContainer = document.createElement("div");
-        resultContainer.classList.add("results-div"); // added to "results-div" class
+        resultContainer.classList.add("results-div");
+
+        // Create a link element with the title as the text content
+        var titleLinkElement = document.createElement("a");
+        titleLinkElement.textContent = result.title;
+        titleLinkElement.href = `chatbot.html?pdfURL=${encodeURIComponent(result.link)}`;
+        titleLinkElement.target = "_blank"; // Opens in a new tab
 
         var buttonContainer = document.createElement("div");
         buttonContainer.classList.add("image-div"); // added to "button-div" class
+
         var titleElement = document.createElement("h3");
-        titleElement.textContent = result.title;
+        titleElement.appendChild(titleLinkElement);
 
         var summaryElement = document.createElement("p");
         summaryElement.innerHTML = "<b>Description:</b> " + result.summary;
@@ -130,14 +136,10 @@ function displayResults(results) {
         var authorsElement = document.createElement("p");
         authorsElement.innerHTML = "<b>Authors:</b> " + result.authors;
 
-        var linkElement = document.createElement("p");
-        linkElement.innerHTML = "<b>Link:</b> " + result.link;
-
         // Append all elements to the result container
         resultContainer.appendChild(titleElement);
         resultContainer.appendChild(summaryElement);
         resultContainer.appendChild(authorsElement);
-        resultContainer.appendChild(linkElement);
 
         // Append the result container to the results container
         resultsContainer.appendChild(resultContainer);
@@ -149,15 +151,11 @@ function displayResults(results) {
         articleImage.alt = "Read Article Image";
         articleImage.addEventListener("click", function() {
             // Redirect to the article link when the image is clicked (temporarily functionalaity and it will be changed to go to chat page) 
-            window.location.href = result.link;
+            window.location.href = `chatbot.html?pdfURL=${encodeURIComponent(result.link)}`;
         });
 
         buttonContainer.appendChild(articleImage);
         buttonsContainer.appendChild(buttonContainer);
     });
 }
-
-
-
-
 
