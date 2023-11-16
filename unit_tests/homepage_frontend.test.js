ec40d4c44eeebee7b0f6f7cba061fe1820abf4b8
Steven-Ao <112782753+Steven-Ao@users.noreply.github.com>
2023-11-15 19:59:35 -0800

    updated comments

--- unit_tests/homepage_frontend.test.js
+++ unit_tests/homepage_frontend.test.js
@@ -1,31 +1,24 @@
-// Import the necessary libraries
 const fs = require('fs');
 const path = require('path');
 const { JSDOM } = require('jsdom');
 
-// Read your HTML file into a string
 const html = fs.readFileSync(path.resolve(__dirname, '../frontend/public/index.html'), 'utf8');
 
-// Initialize the DOM environment with your HTML
 let dom;
 let container;
 
 beforeEach(() => {
-  // Construct a new JSDOM instance with the HTML
   dom = new JSDOM(html, { runScripts: 'dangerously' });
-  // Get the HTML element out of the document
   container = dom.window.document.documentElement;
 });
 
-// Write your test inside a test block
 test('Renders without crashing', () => {
-  // Use expect to make assertions about the HTML elements
+
   expect(container.querySelector('h1').textContent).toBe('ResearchBuddy');
   expect(container.querySelector('#search').placeholder).toBe('Search for papers...');
   expect(container.querySelector('#home_button a').getAttribute('href')).toBe('index.html');
   expect(container.querySelector('#about_button a').textContent).toBe('About');
 
-  // Additional tests
   expect(container.querySelector('title').textContent).toBe('ResearchBuddy');
   expect(container.querySelector('.header')).toBeTruthy();
   expect(container.querySelector('.search-wrapper')).toBeTruthy();
